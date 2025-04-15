54953151277acb099f12485f88599d1e49290b65
José Daniel Grayson <josedanielgrayson@proton.me>
2025-04-15 17:07:43 -0500

    feat: set up calculator base

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
-def calculate() -> float:
-    pass
+line = input("Operación: ")
+
+[lhs, operand, rhs] = line.split()
+
+lhs = float(lhs)
+rhs = float(rhs)
+
+
+def operate(lhs: float, rhs: float, operand: str):
+    return "Operación inválida"
+
+
+result = operate(lhs, rhs, operand)
+print(result)
+
