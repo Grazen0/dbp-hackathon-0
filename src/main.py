930ae67bc49a1c54603417b0af9a9538cc9905f7
José Daniel Grayson <josedanielgrayson@proton.me>
2025-04-15 17:48:49 -0500

    feat: refactor program to work as a function

--- src/main.py
+++ src/main.py
@@ -1,26 +1,19 @@
-line = input("Operación: ")
+def calculate(src: str):
+    [lhs, operand, rhs] = src.split()
 
-[lhs, operand, rhs] = line.split()
+    lhs = float(lhs)
+    rhs = float(rhs)
 
-lhs = float(lhs)
-rhs = float(rhs)
-
-
-def operate(lhs: float, rhs: float, operand: str):
     if operand == "+":
         return lhs + rhs
-      
+
     if operand == "-":
         return lhs - rhs
-      
+
     if operand == "*":
         return lhs * rhs
-      
+
     if operand == "/":
         return lhs / rhs
-      
+
     return "Operación inválida"
-
-
-result = operate(lhs, rhs, operand)
-print(result)
